f50a8131f4869702552d40eec3deff03f7e7aec3
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-26 22:30:13 +0530

    Program to demonstrate array_multisort function in PHP

---
+++ array_multisort.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+***********************************************************
+
+Topic   : Program to demonstrate array_multisort function in PHP.
+Author  : Chpaone09®
+Date    : Oct 26, 2020
+
+************************************************************
+*/
+
+
+$a = array(5,3,4,1,2);
+
+
+array_multisort($a);
+echo "<pre>";print_r($a);
+
+?>
